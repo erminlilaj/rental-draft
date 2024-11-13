2a7482ce3787571831512275f843a8f95317936b
Ermin Lilaj <ermin.lilaj@linksmt.it>
2024-11-13 12:56:57 +0100

    implemented integration tests for userController

--- src/test/java/it/linksmt/rental/RentalApplicationTests.java
+++ src/test/java/it/linksmt/rental/RentalApplicationTests.java
@@ -2,8 +2,12 @@ package it.linksmt.rental;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RentalApplicationTests {
 
 	@Test
